02c72f075e95f8a682a03e53c130b7604b2a0de2
pramod2101 <dpramod1617@gmail.com>
2024-07-15 20:25:29 +0530

    added backend for login page

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -21,3 +21,25 @@ exports.postDetails = (req, res, next) => {
     });
 };
 
+const login= (req,res) =>  {
+    const {email,password}=req.body;
+    if(isstringvalid(emmail) || isstringvalid(password)){
+        return res.status(400).json({message:"email or password is missing", success: false})
+    }
+    console.log(password);
+    Users.findAll({where : {email}}).then(user =>{
+        if(user.length>0){
+            if(user[0].password==password){
+                res.status(200).json({success: true, message:"user logged in successfully"})
+            }
+            else{
+                return res.status(400).json({status:false, message: "password is incorrect"})
+            }
+        }
+        else{
+            return res.status(400).json({status:false, message: "user does not exit"})
+        }
+    }).catch(err =>{
+        res.status(500).json({message:err, success: false})
+    })
+}
